c973679406c59c2ce1179b9faebfc041559c2ff8
Dominic Surano <sk8ingdom@gmail.com>
2014-02-23 18:57:37 -0800

    Org-protocol working
    Abandoned org-mac-protocol and just using command line now.

--- init.el
+++ init.el
@@ -33,17 +33,3 @@
 ;; (load "~/.emacs.d/add-ins/org-protocol")
 (require 'org-protocol)
 (server-start)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(tool-bar-mode nil)
- '(tooltip-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Andale Mono" :foundry "outline" :slant normal :weight normal :height 100 :width normal)))))
--- osx-config/osx.el
+++ osx-config/osx.el
@@ -7,8 +7,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
- '(tool-bar-mode nil))
-
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
