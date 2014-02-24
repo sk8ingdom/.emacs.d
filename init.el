52ab7987727011affa7c98101d47e196a6f48d22
Dominic Surano <sk8ingdom@gmail.com>
2014-02-23 17:50:48 -0800

    Updated OSX Configuration Files
    Created OSX configuration files and adjusted font size and type
    accordingly.

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
  )
  ;; OSX
  ((string-equal system-type "darwin")
+  (load "~/.emacs.d/osx-config/osx")
+  (load "~/.emacs.d/org-mode-config/osx-org")
  )
  ;; Linux
  ((string-equal system-type "gnu/linux")
@@ -31,3 +33,17 @@
 ;; (load "~/.emacs.d/add-ins/org-protocol")
 (require 'org-protocol)
 (server-start)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Andale Mono" :foundry "outline" :slant normal :weight normal :height 100 :width normal)))))
