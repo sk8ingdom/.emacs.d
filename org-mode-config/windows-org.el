05af1c69a0db2241d8ccd13d17e841d32fee2812
Dominic Surano <sk8ingdom@gmail.com>
2014-03-06 10:02:14 -0800

    Update windows-org.el to Forward Slash
    Updated the files paths in windows-org.el to be forward slashes to match those in osx-org.el and linux-org.el and avoid having to use a double backslash for the "U:\\" line as was done in the previous revision.

--- org-mode-config/windows-org.el
+++ org-mode-config/windows-org.el
@@ -1,11 +1,11 @@
 ;; Capture
-(setq org-directory "U:\org")
+(setq org-directory "U:/org")
 (setq org-default-notes-file (concat org-directory "/ref.org"))
 (define-key global-map "\C-cc" 'org-capture)
 
 ;; Targets that contribute to the agenda view
-(setq org-agenda-files (quote ("U:\org")))
+(setq org-agenda-files (quote ("U:/org")))
 
 ;; Abbreviations
 (add-to-list 'org-link-abbrev-alist
-       '("val"            . "U:\\"))
+       '("val"            . "U:/"))
