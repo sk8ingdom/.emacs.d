51d07dff91a5e65f669e4757325745fbd68df3aa
Dominic Surano <sk8ingdom@gmail.com>
2015-10-05 11:31:35 -0700

    Update to osx-gnus.el
    This should HOPEFULLY fix the problems I've been experiencing with using
    Qsync on my MBP.

--- osx-config/osx-gnus.el
+++ osx-config/osx-gnus.el
@@ -1,6 +1,6 @@
 ;; Location of gnutls-cli
-(add-to-list 'exec-path "/usr/local/MacGPG2/bin")
+(add-to-list 'exec-path "/usr/local/MacGPG2/bin/")
 (setq starttls-use-gnutls t)
 
 ;; Location of News, Mail, etc.
-(setq gnus-home-directory "~/dev/Programs/Gnus")
+(setq gnus-home-directory "~/Qsync/dev/Programs/Gnus/")
