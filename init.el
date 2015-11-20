84461453fd40d5aa29cae64a1ccb85cc9cc5b132
Dominic Surano <sk8ingdom@gmail.com>
2015-11-19 16:25:24 -0800

    Update to init.el
    Apparently Emacs 25.5 requires (package-initialize).

--- init.el
+++ init.el
@@ -1,5 +1,11 @@
 ;; -*- mode: emacs-lisp -*-
 
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 ;; General
 (load "~/.emacs.d/general-config/general")
 
