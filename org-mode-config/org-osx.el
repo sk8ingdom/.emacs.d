3fc52ffc6d2392fbcaba948410308e3354367812
Dominic Surano <sk8ingdom@gmail.com>
2016-03-29 23:37:20 -0700

    Update to org-osx.el
    Update to check if using home server and adjusting org-abbreviation
    accordingly. Should work for both wifi and wired connection. Haven’t
    tested wired connection but the IP address shouldn’t change.

--- org-mode-config/org-osx.el
+++ org-mode-config/org-osx.el
@@ -6,6 +6,6 @@
 (setq org-agenda-files (quote ("~/org")))
 
 ;; Abbreviations
-(if (assoc "en0" (network-interface-list))
-    (add-to-list 'org-link-abbrev-alist '("val"     . "file+sys:/Volumes/sk8ingdom/"))
-  (add-to-list 'org-link-abbrev-alist '("val"     . "file+sys:/Volumes/home/")))
+(if (string-equal "[192 168 1 127 0]" (format "%s" (cdar (network-interface-list))))
+    (add-to-list 'org-link-abbrev-alist '("val"     . "file+sys:/Volumes/home/"))
+  (add-to-list 'org-link-abbrev-alist '("val"     . "file+sys:/Volumes/sk8ingdom/")))
