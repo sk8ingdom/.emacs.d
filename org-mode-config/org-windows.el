cb222fc299a57dc89a7e47730f8656a7aeec3b1b
Dominic Surano <sk8ingdom@gmail.com>
2016-05-02 18:38:30 -0700

    Update to org-windows.el
    Changed port for WebDAV access on NAS.

--- org-mode-config/org-windows.el
+++ org-mode-config/org-windows.el
@@ -7,7 +7,7 @@
 
 ;; Abbreviations
 (add-to-list 'org-link-abbrev-alist '("local"   . "U:/"))
-(add-to-list 'org-link-abbrev-alist '("val"     . "http://dominicsurano.com:4041/homes/sk8ingdom/"))
+(add-to-list 'org-link-abbrev-alist '("val"     . "https://dominicsurano.com:8081/homes/sk8ingdom/"))
 ;; (add-to-list 'org-link-abbrev-alist '("outlook" . "C:/Program Files (x86)/Microsoft Office/Office14/OUTLOOK.EXE /select Outlook:"))
 
 ;; Plugins
