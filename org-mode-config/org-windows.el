330e936a612d27b0b8c9904707bf7cf0f723a5d6
Dominic Surano <sk8ingdom@gmail.com>
2016-06-21 17:16:44 -0700

    Update to org-windows.el
    Added mapped network drive which probably overwrites the webDAV link--I
    need to find a way to toggle between them.

--- org-mode-config/org-windows.el
+++ org-mode-config/org-windows.el
@@ -8,6 +8,7 @@
 ;; Abbreviations
 (add-to-list 'org-link-abbrev-alist '("local"   . "U:/"))
 (add-to-list 'org-link-abbrev-alist '("val"     . "https://dominicsurano.com:8081/homes/sk8ingdom/"))
+(add-to-list 'org-link-abbrev-alist '("val"     . "Z:/"))
 ;; (add-to-list 'org-link-abbrev-alist '("outlook" . "C:/Program Files (x86)/Microsoft Office/Office14/OUTLOOK.EXE /select Outlook:"))
 
 ;; Plugins
